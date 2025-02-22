f738a2ce3f7380db4e26651d548a5daa51d163d2
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-22 15:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Get elements
 const video = document.querySelector('video');
 const playerButton = document.querySelector('.player__button');
 const volumeControl = document.querySelector('.volume');
@@ -8,6 +7,17 @@ const progressFilled = document.querySelector('.progress__filled');
 const rewindButton = document.querySelector('.rewind');
 const skipButton = document.querySelector('.skip');
 const speedBar = document.querySelector('.speed-bar');
+
+// Handle update function for controls
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 // Toggle play and pause button
 playerButton.addEventListener('click', () => {
